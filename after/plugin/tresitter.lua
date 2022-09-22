f6da88b2fb496c82b5e469461018fa68bb72fa9e
Shoubhit Dash <shoubhit2005@gmail.com>
2022-09-22 13:49:11 +0530

    feat: add treesitter config

---
+++ after/plugin/tresitter.lua
@@ -0,0 +1,38 @@
+local present, ts = pcall(require, "nvim-treesitter.configs")
+
+if (not present) then 
+  return
+end
+
+ts.setup({
+  auto_install = true,	
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+  matchup = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+  },
+  ensure_installed = {
+    "bash",
+    "css",
+    "elixir",
+    "go",
+    "gomod",
+    "html",
+    "javascript",
+    "json",
+    "lua",
+    "markdown",
+    "prisma",
+    "python",
+    "rust",
+    "toml",
+    "tsx",
+    "typescript",
+    "yaml",
+  },
+})
---
+++ lua/colorscheme.lua
@@ -0,0 +1,5 @@
+vim.g.tokyodark_transparent_background = true
+vim.g.tokyodark_enable_italic = false
+
+vim.cmd("colorscheme tokyodark")
+
