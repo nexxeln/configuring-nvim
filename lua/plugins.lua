db45ba6d991dc495473d1724791863385d088029
Shoubhit Dash <shoubhit2005@gmail.com>
2022-09-25 23:06:43 +0530

    feat: get cmp working

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,6 +4,9 @@ local ensure_packer = function()
   
   if fn.empty(fn.glob(install_path)) > 0 then
     fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
+
+    print("Installing packer...")
+
     vim.cmd [[packadd packer.nvim]]
 
     return true
@@ -13,6 +16,14 @@ local ensure_packer = function()
 end
 
 local packer_bootstrap = ensure_packer()
+
+-- reload neovim whenever plugins.lua is saved
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
 
 return require("packer").startup({ function(use)
   use "wbthomason/packer.nvim"
@@ -37,11 +48,23 @@ return require("packer").startup({ function(use)
   }
 
   -- lsp
-  use "neovim/nvim-lspconfig" 
+  use "neovim/nvim-lspconfig"
 
   -- lsp installer
   use "williamboman/mason.nvim"
   use "williamboman/mason-lspconfig.nvim"
+
+  -- autocompletion
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-buffer"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-nvim-lua"
+  use "hrsh7th/cmp-path"
+  use "saadparwaiz1/cmp_luasnip"
+
+  -- snippets
+  use "L3MON4D3/LuaSnip"
+  use "rafamadriz/friendly-snippets"
 
   if packer_bootstrap then
     require("packer").sync()
@@ -49,7 +72,7 @@ return require("packer").startup({ function(use)
 end,
   config = {
     display = {
-      open_fn = require("packer.util").float,   
+      open_fn = require("packer.util").float,
     },
   },
 }
