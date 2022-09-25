9b12227e91c9f5cbfb5ed5210dcfbe0949b20127
Shoubhit Dash <shoubhit2005@gmail.com>
2022-09-26 00:24:49 +0530

    feat: add cmp client capabilities

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -49,7 +49,7 @@ protocol.CompletionItemKind = {
   "", -- TypeParameter
 }
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 nvim_lsp.tsserver.setup({
@@ -80,7 +80,7 @@ nvim_lsp.jsonls.setup {
   capabilities = capabilities,
 }
 
-nvim_lsp.tailwindcss.setup{}
+nvim_lsp.tailwindcss.setup {}
 
 nvim_lsp.html.setup {
   capabilities = capabilities,
@@ -90,13 +90,13 @@ nvim_lsp.cssls.setup {
   capabilities = capabilities,
 }
 
-nvim_lsp.emmet_ls.setup{}
+nvim_lsp.emmet_ls.setup {}
 
-nvim_lsp.gopls.setup{}
+nvim_lsp.gopls.setup {}
 
-nvim_lsp.marksman.setup{}
+nvim_lsp.marksman.setup {}
 
-nvim_lsp.rust_analyzer.setup{}
+nvim_lsp.rust_analyzer.setup {}
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
