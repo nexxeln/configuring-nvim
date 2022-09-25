af04496b40f3e023aa01c7dfc6914c6f363d1cdf
Shoubhit Dash <shoubhit2005@gmail.com>
2022-09-25 14:34:49 +0530

    feat: add comment for all the modes

--- lua/maps.lua
+++ lua/maps.lua
@@ -3,6 +3,14 @@ local map  = vim.keymap.set
 -- setting leader to space
 vim.g.mapleader = " "
 
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
 -- telescope
 map("n", "<leader>f", ":Telescope find_files<CR>", { silent = true })
 map("n", "<leader>g", ":Telescope live_grep<CR>", { silent = true })
