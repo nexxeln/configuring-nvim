da0aa1945bee40a1d837412f076495a4b5d019f5
Shoubhit Dash <shoubhit2005@gmail.com>
2022-09-25 12:40:38 +0530

    feat: :o

--- lua/maps.lua
+++ lua/maps.lua
@@ -1,7 +1,9 @@
 local map  = vim.keymap.set
 
+-- setting leader to space
 vim.g.mapleader = " "
 
+-- telescope
 map("n", "<leader>f", ":Telescope find_files<CR>", { silent = true })
 map("n", "<leader>g", ":Telescope live_grep<CR>", { silent = true })
 map("n", "<leader>b", ":Telescope buffers<CR>", { silent = true })
